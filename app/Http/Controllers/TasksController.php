bd46d7923ddf0a1cc3301601f2f2ba0aa9850770
keisuke <readmag@feitace.xsrv.jp>
2018-09-11 11:33:22 +0000

    modify TaskController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,7 +47,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content' => 'required:max:191',
-            'status' => 'required|max:191',
+            'status' => 'required|max:10',
             ]);
         
         $task = new Task;
@@ -99,7 +99,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content' => 'required|max:191',
-            'status' => 'required|max:191',
+            'status' => 'required|max:10',
             ]);
         
         $task = Task::find($id);
